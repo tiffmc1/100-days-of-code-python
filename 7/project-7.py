a6441f7c926751d0d20fdfdbcb0bfd02bc568578
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-01-15 22:10:54 -0500

    project 7

---
+++ 7/project-7.py
@@ -0,0 +1,46 @@
+# Project 7 -- Main
+import random
+import hangman_words
+import hangman_art
+
+chosen_word = random.choice(hangman_words.word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+print(hangman_art.logo)
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    # if user has entered a letter they've already guessed
+    if guess in display:
+      print("You've already guessed that letter")
+
+    # check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    # check if user is wrong
+    if guess not in chosen_word:
+        print(f"You guessed letter {guess}, which is incorrect. You're one step closer to losing :)")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(hangman_art.stages[lives])
